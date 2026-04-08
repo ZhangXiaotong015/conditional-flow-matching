951d7af19659c0f35e40d104a3fb9d3f10add63d
ZhangXiaotong015 <xiaotong_z95@163.com>
2026-04-08 15:36:50 +0800

    remote_host

--- my_scripts/run_train.sh
+++ my_scripts/run_train.sh
@@ -4,7 +4,7 @@ python -m examples.C_Arm_Denoising.train_denoising \
        --dataset_name 'dicom_for_comparing' \
        --model 'otcfm' \
        --batch_size 8 \
-       --num_workers 4 \
+       --num_workers 8 \
        --save_step 10000
 
 # sed -i 's/\r$//' run_train.sh
